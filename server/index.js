27336b5581c3107f0fd2509e2b61a2a6079ff435
douglas86 <douglasmaxton@gmail.com>
2022-01-04 22:09:03 +0200

    installed and implemented a few npm packages

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+require('dotenv').config();
 
 // import routes
 import authRoutes from './router/auth';
@@ -6,6 +10,7 @@ import authRoutes from './router/auth';
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes);
 
@@ -15,7 +20,7 @@ app.get('/api/register', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
