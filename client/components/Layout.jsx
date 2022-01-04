b638df6d685ed82e2cc4d197fe9b32f607f72138
douglas86 <douglasmaxton@gmail.com>
2022-01-04 12:42:54 +0200

    used the bootstrap cdn link in the head of Layout

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,18 +1,29 @@
+import Head from 'next/head';
+
 const Layout = ({ children }) => {
+    const head = () => (
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous"
+        />
+    );
+
     const nav = () => (
-        <ul className="nav">
+        <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Home
                 </a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Login
                 </a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="">
+                <a className="nav-link text-dark" href="">
                     Register
                 </a>
             </li>
@@ -20,7 +31,8 @@ const Layout = ({ children }) => {
     );
     return (
         <>
-            {nav()} {children}
+            {head()} {nav()}{' '}
+            <div className="container pt-5 pb-5">{children}</div>
         </>
     );
 };
