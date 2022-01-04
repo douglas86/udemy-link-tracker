1926200b97bcbe228826716b16a483b88c28b4ad
douglas86 <douglasmaxton@gmail.com>
2022-01-04 23:09:43 +0200

    Added validation checks when registering new user

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,13 @@
 import express from 'express';
+
+// import validators
+import { userRegisterValidator } from '../validators/auth';
+import { runValidation } from '../validators';
+
 import { register } from '../controllers/auth';
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', userRegisterValidator, runValidation, register);
 
 export default router;
