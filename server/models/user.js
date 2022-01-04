fda5b77f1b9c6af580798ff7e8dcf7c471e0ab29
douglas86 <douglasmaxton@gmail.com>
2022-01-04 21:50:57 +0200

    Added virtual fields and methods to userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import crypto from 'crypto';
 
 const userSchema = new mongoose.Schema(
     {
@@ -42,5 +43,37 @@ const userSchema = new mongoose.Schema(
 );
 
 // virtual field
+userSchema
+    .virtual('password')
+    .set(function (password) {
+        this._password = password;
+        // generate salt
+        this.salt = this.makeSalt();
+        // encrypt password
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
 // methods > authenticate, encryptPassword, makeSalt
+userSchema.methods = {
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+    makeSalt: function () {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    },
+};
 // export user model
+export default mongoose.model('User', userSchema);
