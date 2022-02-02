df2d4118deecbe97e1272abbf91c75be833ff344
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-02 16:21:49 +0200

    Added getInitialProps to User page

--- client/pages/user/index.jsx
+++ client/pages/user/index.jsx
@@ -1,9 +1,22 @@
-const User = () => {
+import axios from 'axios';
+
+const User = ({ todos }) => {
     return (
         <>
             <h1>Hello User</h1>
+            {JSON.stringify(todos)}
         </>
     );
+};
+
+User.getInitialProps = async () => {
+    const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/todos`
+    );
+
+    return {
+        todos: response.data,
+    };
 };
 
 export default User;
