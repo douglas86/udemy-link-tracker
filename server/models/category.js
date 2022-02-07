43bd7f7099ca627fedff7b39d688059f41cf95e7
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-07 22:07:51 +0200

    Created a category schema for our model

---
+++ server/models/category.js
@@ -0,0 +1,34 @@
+import mongoose, { isValidObjectId } from 'mongoose';
+
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32,
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+            unique: true,
+            index: true,
+        },
+        image: {
+            url: String,
+            key: String,
+        },
+        content: {
+            type: {},
+            min: 20,
+            max: 2000000,
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: 'User',
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Category', categorySchema);
