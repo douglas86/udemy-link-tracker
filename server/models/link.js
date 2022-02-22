1772715eb5288f365ee73ae2768c59015af19595
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-22 15:30:40 +0200

    Created a Link shema

---
+++ server/models/link.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const linkSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 256,
+    },
+    url: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 256,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      required: true,
+      index: true,
+    },
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+    categories: [
+      {
+        type: ObjectId,
+        ref: "Category",
+        required: true,
+      },
+    ],
+    type: {
+      type: String,
+      default: "Free",
+    },
+    medium: {
+      type: String,
+      default: "Video",
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Link", linkSchema);
