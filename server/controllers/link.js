26b6a029a82f4f1bca5668cb7dbb5eeb3296fd41
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-22 20:13:00 +0200

    Link create and read - server

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -4,10 +4,34 @@ import slugify from "slugify";
 // create, list, read, update, remove
 export const create = (req, res) => {
   const { title, url, categories, type, medium } = req.body;
-  console.table({ title, url, categories, type, medium });
+  // console.table({ title, url, categories, type, medium });
+  const slug = url;
+  let link = new Link({ title, url, categories, type, medium, slug });
+  // posted by user
+  link.postedBy = req.user._id;
+  // categories
+  let arrayOfCategories = categories && categories.split(",");
+  link.categories = arrayOfCategories;
+  link.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Link already exist",
+      });
+    }
+    res.json(data);
+  });
 };
 
-export const list = (req, res) => {};
+export const list = (req, res) => {
+  Link.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Could not list links",
+      });
+    }
+    res.json(data);
+  });
+};
 
 export const read = (req, res) => {};
 
