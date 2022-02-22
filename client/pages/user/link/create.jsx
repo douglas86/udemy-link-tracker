3c976824274bb2012a11e0abdfc3cf242bc4346c
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-22 22:21:46 +0200

    Submit link page part 2

--- client/pages/user/link/create.jsx
+++ client/pages/user/link/create.jsx
@@ -37,12 +37,58 @@ const Create = () => {
     setState({ ...state, loadedCategories: response.data });
   };
 
+  const handleSubmit = async (e) => {
+    console.log("e", e);
+  };
+
+  const handleTitleChange = (e) => {
+    setState({ ...state, title: e.target.value, error: "", success: "" });
+  };
+
+  const handleURLChange = (e) => {
+    setState({ ...state, url: e.target.value, error: "", success: "" });
+  };
+
+  // link create form
+  const submitLinkForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="text-muted" htmlFor="">
+          Title
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleTitleChange}
+          value={title}
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted" htmlFor="">
+          URL
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleURLChange}
+          value={url}
+        />
+      </div>
+      <button className="btn btn-outline-warning" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+
   return (
     <div className="row">
       <div className="col-md-12">
         <h1>Submit Link/URL</h1>
         <br />
-        {JSON.stringify(loadedCategories)}
+      </div>
+      <div className="row">
+        <div className="col-md-4">xxx</div>
+        <div className="col-md-8">{submitLinkForm()}</div>
       </div>
     </div>
   );
