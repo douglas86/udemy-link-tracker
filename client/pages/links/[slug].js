f45d7c45842ed87e763cee8c3099c4d01c40acca
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-23 14:36:03 +0200

    Single category page with links part2

--- client/pages/links/[slug].js
+++ client/pages/links/[slug].js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import moment from "moment";
+import { useState } from "react";
 import Link from "next/link";
+import renderHTML from "react-render-html";
 
 const Links = ({
   query,
@@ -9,13 +12,63 @@ const Links = ({
   linksLimit,
   linkSkip,
 }) => {
-  console.log("category", category);
-  console.log("links", JSON.stringify(links));
+  const [allLinks, setAllLinks] = useState(links);
+
+  const listOfLinks = () =>
+    allLinks.map((l, i) => (
+      <div className="row alert alert-primary p-2">
+        <div className="col-md-8">
+          <a href={l.url} target="_blank">
+            <h5 className="pt-2">{l.title}</h5>
+            <h6 className="pt-2 text-danger" style={{ fontSize: "12px" }}>
+              {l.url}
+            </h6>
+          </a>
+        </div>
+        <div className="col-md-4 pt-2">
+          <span className="pull-right">
+            {moment(l.createdAt).fromNow()} by {l.postedBy.name}
+          </span>
+        </div>
+        <div className="col-md-12">
+          <span className="badge text-dark">
+            {l.type} / {l.medium}
+          </span>
+          {l.categories.map((c, i) => (
+            <span className="badge text-success">{c.name}</span>
+          ))}
+        </div>
+      </div>
+    ));
   return (
-    <div className="row">
-      <div className="col-md-8">{JSON.stringify(links)}</div>
-      <div className="col-md-4">right sidebar</div>
-    </div>
+    <>
+      <div className="row">
+        <div className="col-md-8">
+          <h1 className="display-4 font-weight-bold">
+            {category.name} - URL/Links
+          </h1>
+          <div className="lead alert alert-secondary pt-4">
+            {renderHTML(category.content || "")}
+          </div>
+        </div>
+        <div className="col-md-4">
+          <img
+            src={category.image.url}
+            alt={category.name}
+            style={{ width: "auto", maxHeight: "200px" }}
+          />
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-md-8">{listOfLinks()}</div>
+        <div className="col-md-4">
+          <h2 className="lead">Most popular in {category.name}</h2>
+          <p>show popular links</p>
+        </div>
+      </div>
+      <p>load more button</p>
+    </>
   );
 };
 
