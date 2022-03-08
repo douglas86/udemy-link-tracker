48b321ea10bca788e6080b052553b1b0f7585f9d
douglas86 <douglasmaxton@gmail.com>
2022-03-08 18:07:41 +0200

    Categories read - admin

---
+++ client/pages/admin/category/read.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import withAdmin from '../../withAdmin';
+import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
+import Link from 'next/link';
+
+const Read = ({ user, token }) => {
+    const [state, setState] = useState({
+        error: '',
+        success: '',
+        categories: [],
+    });
+
+    const { error, success, categories } = state;
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
+
+    const loadCategories = async () => {
+        const response = await axios.get(
+            `${process.env.NEXT_PUBLIC_API}/categories`
+        );
+        setState({ ...state, categories: response.data });
+    };
+
+    const listCategories = () =>
+        categories.map((c, i) => (
+            <Link href={`/links/${c.slug}`} key={i}>
+                <a
+                    style={{ border: '1px solid red' }}
+                    className="bg-light p-3 col-md-4"
+                >
+                    <div>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <img
+                                    src={c.image && c.image.url}
+                                    alt={c.name}
+                                    style={{ width: '100px', height: 'auto' }}
+                                    className="pr-3"
+                                />
+                            </div>
+                            <div className="col-md-8">
+                                <h3>{c.name}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </Link>
+        ));
+
+    return (
+        <div className="row">
+            <div className="col">
+                <h1>List of categories</h1>
+                <br />
+            </div>
+            <div className="row">{listCategories()}</div>
+        </div>
+    );
+};
+
+export default withAdmin(Read);
--- client/pages/admin/index.jsx
+++ client/pages/admin/index.jsx
@@ -14,6 +14,11 @@ const Admin = ({ user }) => {
                                 <a className="nav-link">Create category</a>
                             </Link>
                         </li>
+                        <li className="nav-item">
+                            <Link href="/admin/category/read">
+                                <a className="nav-link">All categories</a>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="col-md-8"></div>
