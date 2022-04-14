b4ead8541690196add505c9b8ac16a10725c0598
douglas86 <douglasmaxton@gmail.com>
2022-04-14 12:29:11 +0200

    All links for admin - server

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -3,88 +3,97 @@ import slugify from 'slugify';
 
 // create, list, read, update, remove
 export const create = (req, res) => {
-  const { title, url, categories, type, medium } = req.body;
-  // console.table({ title, url, categories, type, medium });
-  const slug = url;
-  let link = new Link({ title, url, categories, type, medium, slug });
-  // posted by user
-  link.postedBy = req.user._id;
-  link.save((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Link already exist',
-      });
-    }
-    res.json(data);
-  });
+    const { title, url, categories, type, medium } = req.body;
+    // console.table({ title, url, categories, type, medium });
+    const slug = url;
+    let link = new Link({ title, url, categories, type, medium, slug });
+    // posted by user
+    link.postedBy = req.user._id;
+    link.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Link already exist',
+            });
+        }
+        res.json(data);
+    });
 };
 
 export const list = (req, res) => {
-  Link.find({}).exec((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Could not list links',
-      });
-    }
-    res.json(data);
-  });
+    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    Link.find({})
+        .populate('postedBy', 'name')
+        .populate('categories', 'name, slug')
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Could not list links',
+                });
+            }
+            res.json(data);
+        });
 };
 
 export const read = (req, res) => {
-  const { id } = req.params;
-  Link.findOne({ _id: id }).exec((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Error finding link',
-      });
-    }
-    res.json(data);
-  });
+    const { id } = req.params;
+    Link.findOne({ _id: id }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Error finding link',
+            });
+        }
+        res.json(data);
+    });
 };
 
 export const update = (req, res) => {
-  const { id } = req.params;
-  const { title, url, category, type, medium } = req.body;
-  const updatedLink = { title, url, category, type, medium };
+    const { id } = req.params;
+    const { title, url, category, type, medium } = req.body;
+    const updatedLink = { title, url, category, type, medium };
 
-  Link.findOneAndUpdate({ _id: id }, updatedLink, { new: true }).exec(
-    (err, updated) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'Error updating the link',
-        });
-      }
-      res.json(updated);
-    }
-  );
+    Link.findOneAndUpdate({ _id: id }, updatedLink, { new: true }).exec(
+        (err, updated) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Error updating the link',
+                });
+            }
+            res.json(updated);
+        }
+    );
 };
 
 export const remove = (req, res) => {
-  const { id } = req.params;
-  Link.findOneAndRemove({ _id: id }).exec((err, data) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Error removing the link',
-      });
-    }
-    res.json({
-      message: 'Link removed successfully',
+    const { id } = req.params;
+    Link.findOneAndRemove({ _id: id }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Error removing the link',
+            });
+        }
+        res.json({
+            message: 'Link removed successfully',
+        });
     });
-  });
 };
 
 export const clickCount = (req, res) => {
-  const { linkId } = req.body;
-  Link.findByIdAndUpdate(
-    linkId,
-    { $inc: { clicks: 1 } },
-    { upsert: true, new: true }
-  ).exec((err, result) => {
-    if (err) {
-      return res.status(400).json({
-        error: 'Could not update view count',
-      });
-    }
-    res.json(result);
-  });
+    const { linkId } = req.body;
+    Link.findByIdAndUpdate(
+        linkId,
+        { $inc: { clicks: 1 } },
+        { upsert: true, new: true }
+    ).exec((err, result) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Could not update view count',
+            });
+        }
+        res.json(result);
+    });
 };
--- server/router/link.js
+++ server/router/link.js
@@ -7,35 +7,39 @@ import { linkCreateValidator, linkUpdateValidator } from '../validators/link';
 import { runValidation } from '../validators';
 
 // controllers
-import { requireSignIn, authMiddleWare } from '../controllers/auth';
 import {
-  create,
-  list,
-  read,
-  update,
-  remove,
-  clickCount,
+    requireSignIn,
+    authMiddleWare,
+    adminMiddleWare,
+} from '../controllers/auth';
+import {
+    create,
+    list,
+    read,
+    update,
+    remove,
+    clickCount,
 } from '../controllers/link';
 
 // routes
 router.post(
-  '/link',
-  linkCreateValidator,
-  runValidation,
-  requireSignIn,
-  authMiddleWare,
-  create
+    '/link',
+    linkCreateValidator,
+    runValidation,
+    requireSignIn,
+    authMiddleWare,
+    create
 );
-router.get('/links', list);
+router.post('/links', requireSignIn, adminMiddleWare, list);
 router.put('/click-count', clickCount);
 router.get('/link/:id', read);
 router.put(
-  '/link/:id',
-  linkUpdateValidator,
-  runValidation,
-  requireSignIn,
-  authMiddleWare,
-  update
+    '/link/:id',
+    linkUpdateValidator,
+    runValidation,
+    requireSignIn,
+    authMiddleWare,
+    update
 );
 router.delete('/link/:id', requireSignIn, authMiddleWare, remove);
 
